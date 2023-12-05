e3f4f45fe1008154b529272b3abe7e62101ea1a9
rei234555 <reinantosaputra@gmail.com>
2023-12-05 23:52:21 +0700

    modul 8

---
+++ src/screens/index.js
@@ -0,0 +1,11 @@
+import Home  from "./Home";
+import Profile from './Profile'
+import Add from './Add'
+import Register from './Register'
+import Splash from './Splash'
+import EditNote from "./EditNote"
+import Login from "./Login/index ";
+
+
+
+export {Home, Profile, Add, Login, Register, Splash, EditNote}
